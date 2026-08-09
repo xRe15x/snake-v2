0535282ff61807e11164710904013ae2ea319ed5
xRe15x <266321938+xRe15x@users.noreply.github.com>
2026-08-09 18:27:51 +0100

    main controller for game.js and ui.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import "./style.css";
-import game from "./game.js";
+import createGame from "./game";
+import createUI from "./ui.js"
+
+const game = createGame();
+const ui = createUI();
+
+function gameLoop() {
+    requestAnimationFrame(gameLoop);
+
+    game.update();
+    ui.update();
+};
+
+requestAnimationFrame(gameLoop);
 
 console.log("JS running!");

2383dedcf154e04dd365d99db24f72c9455ac561
xRe15x <266321938+xRe15x@users.noreply.github.com>
2026-08-09 14:48:03 +0100

    frame func

---
+++ src/game.js
@@ -0,0 +1,13 @@
+// MAKE SURE TO SEPARATE GAME LOGIC FROM UI, AND USE LERP TO MOVE SNAKE
+
+function game() {
+    function frame() {
+        requestAnimationFrame(frame); // Recursive call to repeat frames
+    };
+
+    return {};
+};
+
+function gameUI() {};
+
+export default game;

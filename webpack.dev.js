c88d66bf852bd348677b5223f82f6abbc2fac7fe
xRe15x <266321938+xRe15x@users.noreply.github.com>
2026-08-09 12:01:44 +0000

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+import common from "./webpack.common.js"
+import {merge} from "webpack-merge";
+
+export default merge(common, {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+        watchFiles: ["./src/index.html"]
+    }
+});

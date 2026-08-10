0b97942c8f1743401bf37f772f695f4226aec39d
xRe15x <266321938+xRe15x@users.noreply.github.com>
2026-08-10 15:23:34 +0100

    changed snake colour + score UI

--- src/ui.js
+++ src/ui.js
@@ -2,13 +2,16 @@ function createUI(gridSize) {
     const canvas = document.getElementById("game");
     const ctx = canvas.getContext("2d");
 
+    const scoreText = document.getElementById("score");
+    const bestScoreText = document.getElementById("best-score");
+
     const cellX = canvas.width / gridSize.x;
     const cellY = canvas.height / gridSize.y;
 
     // Private functions
     function drawSnake(segments) {
         segments.forEach(pos => {
-            ctx.fillStyle = "rgb(108, 255, 108)";
+            ctx.fillStyle = "rgb(252, 252, 252)";
             ctx.fillRect(cellX * pos.x + 0.5, cellY * pos.y + 0.5, cellX - 1, cellY - 1);
         });
     };
@@ -22,7 +25,7 @@ function createUI(gridSize) {
 
     // Public functions
     function update(gameData) {
-        const {playerSegments, applePosition} = gameData;
+        const {playerSegments, applePosition, score} = gameData;
 
         // Clear last frame
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -30,6 +33,9 @@ function createUI(gridSize) {
         // Draw everything
         drawSnake(playerSegments);
         drawApple(applePosition());
+
+        // Update UI
+        scoreText.textContent = "Score: " + score;
     };
 
     return {update};

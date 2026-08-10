b648b2a58c08cf07dae18c8008e7f0fb7b6d0ad6
xRe15x <266321938+xRe15x@users.noreply.github.com>
2026-08-11 00:11:01 +0100

    some size changes for snake and apple cells

--- src/ui.js
+++ src/ui.js
@@ -2,25 +2,45 @@ function createUI(gridSize) {
     const canvas = document.getElementById("game");
     const ctx = canvas.getContext("2d");
 
-    const scoreText = document.getElementById("score");
-    const bestScoreText = document.getElementById("best-score");
-
+    // Get X and Y size of a cell
     const cellX = canvas.width / gridSize.x;
     const cellY = canvas.height / gridSize.y;
 
+    const scoreText = document.getElementById("score");
+    //const bestScoreText = document.getElementById("best-score");
+
     // Private functions
+
+    // Make a function to get the css colours ////////////////////////////////////
+
+    function drawMap() { // Draw the checkered background
+        const shrink = 4; // Amount to shrink each cell so the background colour slightly reveals
+        for (let x = 0; x < gridSize.x; x++) {
+            for (let y = 0; y < gridSize.y; y++) {
+                if ((x + y) % 2 === 0) {
+                    ctx.fillStyle = "rgb(51, 51, 51)"
+                } else {
+                    ctx.fillStyle = "rgb(36, 36, 36)";
+                }
+                ctx.fillRect(cellX * x + shrink / 2, cellY * y + shrink / 2, cellX - shrink, cellY - shrink); // Fill cells and apply shrink
+            }
+        }
+    };
+
     function drawSnake(segments) {
+        const shrink = 2;
         segments.forEach(pos => {
             ctx.fillStyle = "rgb(252, 252, 252)";
-            ctx.fillRect(cellX * pos.x + 0.5, cellY * pos.y + 0.5, cellX - 1, cellY - 1);
+            ctx.fillRect(cellX * pos.x + shrink / 2, cellY * pos.y + shrink / 2, cellX - shrink, cellY - shrink);
         });
     };
 
     function drawApple(position) {
         if (position === undefined) return; // Apple has not spawned yet
 
+        const shrink = 10;
         ctx.fillStyle = "rgb(255,0,0)";
-        ctx.fillRect(cellX * position.x + 2.5, cellY * position.y + 2.5, cellX - 5, cellY - 5)
+        ctx.fillRect(cellX * position.x + shrink / 2, cellY * position.y + shrink / 2, cellX - shrink, cellY - shrink)
     }
 
     // Public functions
@@ -31,10 +51,11 @@ function createUI(gridSize) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         // Draw everything
+        drawMap();
         drawSnake(playerSegments);
         drawApple(applePosition());
 
-        // Update UI
+        // Update scores
         scoreText.textContent = "Score: " + score;
     };
 
